78c1adbb8b6d48f7875fc10e398556b903f5a68e
Umeshwaran Mohan <umeshwaran.mohan@telusinternational.com>
2023-02-27 16:27:56 +0530

    first

---
+++ src/main/java/com/ABCcompany/employeesGroup/EmployeesGroupApplication.java
@@ -0,0 +1,49 @@
+package com.ABCcompany.employeesGroup;
+
+import java.io.InputStream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ABCcompany.employeesGroup.service.EmployeeService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+import com.ABCcompany.employeesGroup.model.Employee;
+
+import java.util.List;
+
+
+
+@SpringBootApplication
+public class EmployeesGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesGroupApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(EmployeeService employeeService){
+		return args  ->  {
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+
+			TypeReference<List<Employee>> typeReference = new TypeReference<List<Employee>>() {};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/employee.json");
+			try{
+				List<Employee> employee = mapper.readValue(inputStream, typeReference);
+				employeeService.saveEmployee((Employee) employee);
+				System.out.println("Employee saved");
+			}catch (Exception e) {
+				System.out.println("Unable to save"+ e.getMessage());
+			}
+
+		
+	};
+}
+}
---
+++ src/main/java/com/ABCcompany/employeesGroup/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.ABCcompany.employeesGroup.service;
+
+import java.util.List;
+
+import com.ABCcompany.employeesGroup.model.Employee;
+
+public interface EmployeeService {
+
+	Employee saveEmployee(Employee employee);
+	List<Employee> getAllEmployees();
+	
+	List<Employee> getFilteredEmployees(String married, Integer salary);
+	
+}
+	
